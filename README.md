1488a165d2b9f3f94ea8c8314fa55f01b14958f8
Ric69 <spaaro69@gmail.com>
2021-04-15 20:51:21 +0200

    commit initial branch master

---
+++ README.md
@@ -0,0 +1,21 @@
+Mapstore dev pour home
+==========
+
+Quick Start
+------------
+
+Clone the repository with the --recursive option to automatically clone submodules:
+
+`git clone --recursive https://gitlab.com/dev-cnr/mapstore-de-dev.git`
+
+Install NodeJS >= 7.10.0 , if needed, from [here](https://nodejs.org/en/download/releases/).
+
+Start the development application locally:
+
+`npm install`
+
+`npm start`
+
+The application runs at `http://localhost:8081` afterwards.
+
+Read more on the [wiki](https://gitlab.com/dev-cnr/mapstore-de-dev.git/wiki).
---
+++ webpack.config.devhome.js
@@ -0,0 +1,64 @@
+const path = require("path");
+
+const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
+const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
+const ModuleFederationPlugin = require('./MapStore2/build/moduleFederation').plugin;
+
+module.exports = require('./MapStore2/build/buildConfig')(
+    {
+        'devhome': path.join(__dirname, "js", "app"),
+        'devhome-embedded': path.join(__dirname, "js", "embedded"),
+        'devhome-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api"),
+        'geostory-embedded': path.join(__dirname, "js", "geostoryEmbedded"),
+        "dashboard-embedded": path.join(__dirname, "js", "dashboardEmbedded")
+    },
+    themeEntries,
+    {
+        base: __dirname,
+        dist: path.join(__dirname, "dist"),
+        framework: path.join(__dirname, "MapStore2", "web", "client"),
+        code: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
+    },
+    [extractThemesPlugin, ModuleFederationPlugin],
+    false,
+    "dist/",
+    '.devhome',
+    [],
+    {
+        "@mapstore": path.resolve(__dirname, "MapStore2", "web", "client"),
+        "@js": path.resolve(__dirname, "js")
+    },{
+        '/rest/geostore': {
+            target: 'http://localhost:8080/dev-home',
+            secure: false,
+            headers: {
+                host: '127.0.0.1:8080',
+            },
+        },
+        '/pdf': {
+            target: 'http://localhost:8080/geoserver',
+            secure: false,
+            headers: {
+                host: '127.0.0.1:8080',
+            },
+        },
+        '/mapstore/pdf': {
+            target: 'http://localhost:8080/geoserver',
+            secure: false,
+            headers: {
+                host: '127.0.0.1:8080',
+            },
+        },
+        '/proxy': {
+            target: 'http://localhost:8080/dev-home',
+            secure: false,
+            headers: {
+                host: '127.0.0.1:8080',
+            },
+        },
+        '/docs': {
+            target: 'http://localhost:8081',
+            pathRewrite: { '/docs': '/mapstore/docs' },
+        },
+    }
+);
